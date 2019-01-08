4b0c6e21ffdb7625406c86fa8dc70d29434dc9e5
David <xodnjs2549@naver.com>
2019-01-08 17:39:03 +0900

    add userService upgradeLevels method
    If user reach specific count about login and recommend, level will
    upgrade

--- src/main/java/hardlearner/springStudy/user/service/UserService.java
+++ src/main/java/hardlearner/springStudy/user/service/UserService.java
@@ -1,5 +1,6 @@
 package hardlearner.springStudy.user.service;
 
+import hardlearner.springStudy.user.dao.Level;
 import hardlearner.springStudy.user.dao.UserDao;
 import hardlearner.springStudy.user.domain.User;
 
@@ -10,5 +11,24 @@ public class UserService {
 
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
+    }
+
+    public void upgradeLevels() {
+        List<User> users = userDao.getAll();
+        for (User user : users) {
+            Boolean changed = false;
+            if (user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
+                user.setLevel(Level.SILVER);
+                changed = true;
+            } else if (user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
+                user.setLevel(Level.GOLD);
+                changed = true;
+            } else if (user.getLevel() == Level.GOLD) {
+                changed = false;
+            }
+            if (changed) {
+                userDao.update(user);
+            }
+        }
     }
 }
--- src/test/java/hardlearner/springStudy/user/service/UserServiceTest.java
+++ src/test/java/hardlearner/springStudy/user/service/UserServiceTest.java
@@ -1,21 +1,61 @@
 package hardlearner.springStudy.user.service;
 
+import hardlearner.springStudy.user.dao.Level;
+import hardlearner.springStudy.user.dao.UserDao;
+import hardlearner.springStudy.user.domain.User;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertNotNull;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(locations = "/applicationContext.xml")
 public class UserServiceTest {
     @Autowired
-    UserService userService;
+    private UserService userService;
+    @Autowired
+    private UserDao userDao;
+
+    private List<User> users;
+
+    @Before
+    public void setUp() {
+        users = Arrays.asList(
+                new User("bumjin", "박범진", "p1", Level.BASIC, 49, 0),
+                new User("joytouch", "강명성", "p2", Level.BASIC, 50, 0),
+                new User("erwins", "신승한", "p3", Level.SILVER, 60, 29),
+                new User("madnite1", "이상호", "p4", Level.SILVER, 60, 30),
+                new User("green", "오민규", "p5", Level.GOLD, 100, 100)
+        );
+
+        userDao.deleteAll();
+    }
 
     @Test
-    public void bean() {
-        assertNotNull(userService);
+    public void upgradeLevels() {
+        for (User user : users) {
+            userDao.add(user);
+        }
+
+        userService.upgradeLevels();
+
+        checkLevel(users.get(0), Level.BASIC);
+        checkLevel(users.get(1), Level.SILVER);
+        checkLevel(users.get(2), Level.SILVER);
+        checkLevel(users.get(3), Level.GOLD);
+        checkLevel(users.get(4), Level.GOLD);
+    }
+
+    private void checkLevel(User user, Level expectedLevel) {
+        User userUpdate = userDao.get(user.getId());
+        assertThat(userUpdate.getLevel(), is(expectedLevel));
     }
 }
