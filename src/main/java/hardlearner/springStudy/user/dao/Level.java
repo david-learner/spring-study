7e1672184227ed4209676a8ec8d6d36ef89de0c0
David <xodnjs2549@naver.com>
2019-01-08 16:05:41 +0900

    add Level enum, login & recommend count
    This is for User Level
    add login & recommend property to User class and DB column

---
+++ src/main/java/hardlearner/springStudy/user/dao/Level.java
@@ -0,0 +1,32 @@
+package hardlearner.springStudy.user.dao;
+
+public enum Level {
+
+    BASIC(1),
+    SILVER(2),
+    GOLD(3);
+
+
+    private final int level;
+
+    Level(int level) {
+        this.level = level;
+    }
+
+    public int intValue() {
+        return this.level;
+    }
+
+    public static Level valueOf(int value) {
+        switch (value) {
+            case 1:
+                return BASIC;
+            case 2:
+                return SILVER;
+            case 3:
+                return GOLD;
+            default:
+                throw new AssertionError("Unknown value: " + value);
+        }
+    }
+}
---
+++ src/main/resources/ddl.sql
@@ -0,0 +1,5 @@
+/* DB : springbook  */
+
+ALTER TABLE users ADD level tinyint not null;
+ALTER TABLE users ADD login int not null;
+ALTER TABLE users ADD recommend int not null;
