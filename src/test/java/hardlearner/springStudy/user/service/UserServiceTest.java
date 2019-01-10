64e6b3917e8579d23137a879eeb15288b67854bb
David <xodnjs2549@naver.com>
2019-01-10 15:44:18 +0900

    add mock object for MailSender
    mock can check method internal working
    On this, mock shows me whom send the mail

--- src/test/java/hardlearner/springStudy/user/service/UserServiceTest.java
+++ src/test/java/hardlearner/springStudy/user/service/UserServiceTest.java
@@ -7,20 +7,22 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static hardlearner.springStudy.user.service.UserService.MIN_LOGCOUNT_FOR_SILVER;
 import static hardlearner.springStudy.user.service.UserService.MIN_RECOMMEND_FOR_GOLD;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(locations = "/applicationContext.xml")
@@ -50,10 +52,14 @@ public class UserServiceTest {
     }
 
     @Test
+    @DirtiesContext
     public void upgradeLevels() throws Exception {
         for (User user : users) {
             userDao.add(user);
         }
+
+        MockMailSender mockMailSender = new MockMailSender();
+        userService.setMailSender(mockMailSender);
 
         userService.upgradeLevels();
 
@@ -62,6 +68,12 @@ public class UserServiceTest {
         checkLevel(users.get(2), false);
         checkLevel(users.get(3), true);
         checkLevel(users.get(4), false);
+
+        List<String> requests = mockMailSender.getRequests();
+        assertThat(requests.size(), is(2));
+        assertThat(requests.get(0), is(users.get(1).getEmail()));
+        assertThat(requests.get(1), is(users.get(3).getEmail()));
+
     }
 
     private void checkLevel(User user, boolean upgrade) {
@@ -112,6 +124,19 @@ public class UserServiceTest {
         checkLevel(users.get(1), false);
     }
 
+    @Test
+    public void checkEMail() {
+        for (User user : users) {
+            userDao.add(user);
+        }
+
+        users = userDao.getAll();
+
+        for (User user : users) {
+            assertNotNull(user.getEmail());
+        }
+    }
+
     static class TestUserService extends UserService {
         private String id;
 
@@ -128,18 +153,22 @@ public class UserServiceTest {
         }
     }
 
-    @Test
-    public void checkEMail() {
-        for (User user : users) {
-            userDao.add(user);
+    static class MockMailSender implements MailSender {
+
+        private List<String> requests = new ArrayList<>();
+
+        public List<String> getRequests() {
+            return this.requests;
         }
 
-        users = userDao.getAll();
+        @Override
+        public void send(SimpleMailMessage simpleMailMessage) throws MailException {
+            requests.add(simpleMailMessage.getTo()[0]); // 수신자(받는 사람) 목록 중 첫 번째
+        }
 
-        for (User user : users) {
-            assertNotNull(user.getEmail());
+        @Override
+        public void send(SimpleMailMessage... simpleMailMessages) throws MailException {
+
         }
     }
-
-
 }
