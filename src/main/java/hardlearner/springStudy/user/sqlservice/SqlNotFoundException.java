d014fee13cbe74bd50b26892aa7e7606f2444127
David <xodnjs2549@naver.com>
2019-02-01 16:24:57 +0900

    Add SqlRegistry, SqlReader
    XmlSqlService has three concern so seperated those
    To dependency relation, sqlService bean refer to oneself as sqlReader,
    sqlRegistry
    The reason that refer to oneself is XmlSqlService did implement each
    interface

---
+++ src/main/java/hardlearner/springStudy/user/sqlservice/SqlNotFoundException.java
@@ -0,0 +1,7 @@
+package hardlearner.springStudy.user.sqlservice;
+
+public class SqlNotFoundException extends RuntimeException {
+    public SqlNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/hardlearner/springStudy/user/sqlservice/SqlRegistry.java
@@ -0,0 +1,7 @@
+package hardlearner.springStudy.user.sqlservice;
+
+public interface SqlRegistry {
+    void registerSql(String key, String sql);
+
+    String findSql(String key) throws SqlNotFoundException;
+}
